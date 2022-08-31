fa7f53beaa63dda6435467ee093b3eab2ceeb11e
takuori <104710179+takuori@users.noreply.github.com>
2022-08-31 09:30:40 +0000

    [3rdCommit]new/show/edit[fix]

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,40 @@ class PostsController < ApplicationController
   def show
     @post = Post.find_by(id: params[:id])
   end
+  
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(content: params[:content])
+    if @post.save
+      flash[:notice] = "投稿に成功しました"
+      redirect_to "/posts/index"
+    else
+      render "posts/new"
+    end
+  end
+  
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+  
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    if @post.save
+      flash[:notice] = "更新しました"
+      redirect_to "/posts/index"
+    else
+      render "posts/edit"
+    end
+  end
+  
+  def destroy
+    @post = Post.find_by(id: params[:id]) 
+    @post.destroy
+    flash[:notice] = "削除しました"
+    redirect_to "/posts/index"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   get '/' => 'home#top'
   get 'about' => 'home#about'
   get 'posts/index' => 'posts#index'
+  get 'posts/new' => 'posts#new'
   get 'posts/:id' => 'posts#show'
+  post 'posts/create' => 'posts#create'
+  get 'posts/:id/edit' => 'posts#edit'
+  post 'posts/:id/update' => 'posts#update'
+  post 'posts/:id/destroy' => 'posts#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
