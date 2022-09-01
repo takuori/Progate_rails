4b63e56c2112d2e74a5bc80c95fa142eb2e5eecb
takuori <104710179+takuori@users.noreply.github.com>
2022-09-01 08:33:23 +0000

    細かな修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  get 'signup' => 'users#new'
-  post 'users/create' => 'users#create'
-  get 'users/index' => 'users#index'
-  get 'users/:id' => 'users#show'
-  get 'users/:id/edit' => 'users#edit'
-  get 'users/:id/update' => 'users#update'
+  get "login" => "users#login_form"
+  post "login" => "users#login"
+  post "logout" => "users#logout"
+  post "users/:id/update" => "users#update"
+  get "users/:id/edit" => "users#edit"
+  post "users/create" => "users#create"
+  get "signup" => "users#new"
+  get "users/index" => "users#index"
+  get "users/:id" => "users#show"
   
   get '/' => 'home#top'
   get 'about' => 'home#about'
